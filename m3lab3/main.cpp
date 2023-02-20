16a10a62cd574479918bdf3fda9631e9e8e72a95
teacherbot <norrisa@faytechcc.edu>
2023-02-20 11:07:18 -0500

    updated craps program

--- m3lab3/main.cpp
+++ m3lab3/main.cpp
@@ -11,20 +11,34 @@ using namespace std;
 /*
 This program simulates a single game of craps.
 First version (MVP) is a single roll.
-
 */
 
 int main() {
   // variables
   int die1, die2, roll;   // roll is 2d6
   int seed;               // will use time(0) later
-  //char any_key;           // for cin.get()
+
+  
   bool winner = false;
   cout << "Welcome to the craps table!\n";
 
   // For testing, we'll use a repeatable seed
-  cout << "What's your lucky number? ";
-  cin >> seed;
+  /*
+    cout << "do you like dice games? " ; 
+  string answer;
+  cin >> answer;
+  if (answer =="yes") {
+    cout << "Great, let's roll the bones!"<< endl;
+  }
+  else {
+    cout << "Sorry, I don't know how to do card games in c++ yet" << endl;
+  }
+  */
+  //cout << "What's your lucky number? ";
+  //cin >> seed;
+  // seed is truly random
+  seed = time(0); 
+  //cout << "DEBUG: time(0) = " << seed << endl;
   srand(seed);
   
   // start game
@@ -33,33 +47,38 @@ int main() {
   die2 = (rand() % 6) + 1;
   roll = die1 + die2;
   // DEBUG: just use lucky number as roll
-  roll = seed; // take this out!
+  //roll = seed; // take this out!
 
   cout << "You rolled: (" << die1 << " + " << die2 << ") -> ";
   cout << roll << endl;
 
-  // determine outcome!
+  // determine outcome!  
   switch (roll) {
     //  instant losses
     case 2:
-      cout << "Snake eyes!" << endl;
+      cout << "Two - Snake eyes!" << endl;
       winner = false;
       break;
     case 12:
-      cout << "Boxcars!" << endl;
+      cout << "Twelve - Boxcars!" << endl;
       winner = false;
       break;
 
     // instant wins
     case 7: 
-      cout << "You win!" << endl;
+      cout << "Lucky Number Seven!" << endl;
       winner = true;
       break;
-    // TODO: add 11
+    case 11:
+      cout << "Eleven! Eleven's a winner!" << endl;
+      break;
     
     // to be continued (point rolls)
     default:
-      cout << "Unknown - TODO" << endl;
+      cout << "Rolled: " << roll << endl;
+      cout << "Continue playing, point is " << roll << endl;
+      cout << "But we'll just call it a draw." << endl;
+      // we should have a subroutine (function) for this
       break;
   }
   if (winner) {
