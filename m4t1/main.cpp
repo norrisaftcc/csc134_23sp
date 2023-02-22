411e9ea6a4d22b73853d3290b0f1af9bbcf1d4a7
teacherbot <norrisa@faytechcc.edu>
2023-02-22 11:21:54 -0500

    part 2 (table of squares)

--- m4t1/main.cpp
+++ m4t1/main.cpp
@@ -21,11 +21,34 @@ void say_hello(int count) {
   return; // for void functions, return is optional (implied)
 }
 
+int square(int num) {
+  // input: num, an integer
+  // output: num squared
+  return num*num;
+}
+
 int main() {
   // call a function
   int times = 0;
   cout << "How many times should I say Hello? ";
   cin >> times;
   say_hello(times); 
+
+  // part 2 -- table of squares
+  // The world’s first “real program” running on a stored-program 
+  // computer (David Wheeler, Cambridge, May 6, 1949)
+  
+  int i = 0;
+  int max;
+  cout <<  "How many numbers in the table of squares? ";
+  cin >> max;
+  cout << "Table of squares from 0 to " << max << endl;
+  cout << "Number" << "\t" << "Square" << endl;
+  cout << "____________________" << endl;
+  while (i <= max) {
+    cout << i << "\t\t" << square(i) << endl;
+    i++; // or i = i + 1
+  }
+
   
 }
