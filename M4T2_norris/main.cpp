72e7efa5cf1d60be0a10084d4a9f0455150b7e87
teacherbot <norrisa@faytechcc.edu>
2023-02-27 11:10:47 -0500

    started project

---
+++ M4T2_norris/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// CSC 134
+// M4T2 - Menus
+// Name
+// Date
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    // Print three line menu
+    // press 1 to blah etc
+    // cin an integer
+
+    return 0;
+}
