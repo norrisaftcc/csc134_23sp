6b5d6a7404bfa240f51c78c8b01a2b1670de93f4
teacherbot <norrisa@faytechcc.edu>
2023-03-01 11:23:30 -0500

    Started version2
    This is our character creator.

--- M4T2_norris/main.cpp
+++ M4T2_norris/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <cstdlib>  // Enables rand(), srand()
+#include <ctime>    // Enables use of time() function
 using namespace std;
 // CSC 134
 // M4T2 - Menus
 // Name
 // Date
+// Version 2 -- started our character creator
+// for Thudd
+
 
 // menu functions
 void option_one() {
@@ -24,31 +29,37 @@ void option_one() {
 }
 
 void option_two() {
-    // sum up five numbers
+    // dice roller
+    int d6_1, d6_2, d6_3; // 3 dice (6 sided)
+    int total; // 3d6
+    int seed;
+
 }
 
 void option_three() {
-    // TODO: what does this even do
+    // sum and average of dice
 }
+
+//  There is no option 4 function, it's just quit.
 
 // main menu
 int main()
 {
     // display a menu.
-    // have the user choose 1, 2, or 3.
+    // have the user choose 1, 2, 3, or 4.
     // do something based on the choice
 
     cout << "Welcome to the menu" << endl;
     cout << "Press 1 to count" << endl;
-    cout << "Press 2 to sum up numbers" << endl;
-    cout << "Press 3 to TODO" << endl; // not implemented yet
+    cout << "Press 2 to roll dice" << endl;
+    cout << "Press 3 to roll a character" << endl; // not implemented yet
     cout << "Choice: ";
     // input validation
     int choice;
     cin >> choice;
-    // user must pick 1, 2, or 3
-    while (choice < 1 || choice > 3) {
-        cout << "Please choose 1, 2, or 3: ";
+    // user must pick 1, 2, 3, or 4
+    while (choice < 1 || choice > 4) {
+        cout << "Please choose 1, 2, 3, or 4: ";
         cin >> choice;
     }
     // if we get here, we know choice is valid
